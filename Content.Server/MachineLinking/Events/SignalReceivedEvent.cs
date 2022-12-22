6ec30f759e312e6cb8c222f57ac6372269ba3b54
Kevin Zheng <kevinz5000@gmail.com>
2022-12-21 16:39:11 -0800

    Add stateful machine links

--- Content.Server/MachineLinking/Events/SignalReceivedEvent.cs
+++ Content.Server/MachineLinking/Events/SignalReceivedEvent.cs
@@ -1,12 +1,21 @@
 namespace Content.Server.MachineLinking.Events
 {
+    public enum SignalState
+    {
+        Momentary,
+        Low,
+        High
+    }
+
     public sealed class SignalReceivedEvent : EntityEventArgs
     {
         public readonly string Port;
+        public readonly SignalState State;
 
-        public SignalReceivedEvent(string port)
+        public SignalReceivedEvent(string port, SignalState state)
         {
             Port = port;
+            State = state;
         }
     }
 }
