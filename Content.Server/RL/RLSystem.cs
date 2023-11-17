d8ab302b3f5a7d402552ce9843b197d30939ac0e
Kevin Zheng <kevinz5000@gmail.com>
2023-11-17 15:51:48 -0800

    Convert RL to use gRPC

--- Content.Server/RL/RLSystem.cs
+++ Content.Server/RL/RLSystem.cs
@@ -1,125 +1,43 @@
-using Content.Server.Administration;
-using Content.Shared.Administration;
-using Robust.Shared.Console;
-using Robust.Shared.Log;
+using Grpc.Net.Client;
+
+using Content.Server.RL;
+using Content.Shared.CCVar;
+using Robust.Shared.Configuration;
 
 public class RLSystem : EntitySystem
 {
-    private string entryPoint = "Resources/Mining/RL/init.lisp";
+    [Dependency] private readonly IConfigurationManager _cfg = default!;
 
-    private bool available = false;
+    private GrpcChannel? Channel;
 
     public override void Initialize()
     {
-#if RL
-        try
-        {
-            base.Initialize();
-            RL.ecl_set_option(1, 0); // TRAP_SIGSEGV = false
-            RL.ecl_set_option(2, 0); // TRAP_SIGFPE = false
-            RL.ecl_set_option(3, 0); // TRAP_SIGINT = false
-            RL.ecl_set_option(4, 0); // TRAP_SIGILL = false
-            RL.ecl_set_option(5, 0); // TRAP_SIGBUS = false
-            RL.ecl_set_option(6, 0); // TRAP_SIGPIPE = false
-            if (RL.boot() == 0)
-            {
-                throw new Exception("Failed to start; could not boot");
-            }
-            Logger.DebugS("RL", "Started RL");
-            available = true;
-            Reload();
-        }
-        catch (TypeInitializationException e)
-        {
-            Logger.ErrorS("RL", "Failed to start; could not find libRL");
-        }
-#else
-        Logger.WarningS("RL", "Not available in this build");
-#endif
+        var endpoint = _cfg.GetCVar(CCVars.RLEndpoint);
+        if (endpoint == string.Empty)
+            return;
+
+        Channel = GrpcChannel.ForAddress(endpoint);
     }
 
     public bool Available()
     {
-        return available;
+        return Channel is not null;
     }
 
-    public string Reload()
+    public RL.RLClient Client()
     {
-#if RL
-        return RL.repr(RL.eval_str($"(load \"{entryPoint}\")"));
-#else
-        return "Not available in this build";
-#endif
+        return new RL.RLClient(Channel);
+    }
+
+    public DateTime Deadline()
+    {
+        return DateTime.UtcNow.AddSeconds(_cfg.GetCVar(CCVars.RLTimeout));
     }
 
     public override void Shutdown()
     {
         base.Shutdown();
-#if RL
-        RL.cl_shutdown();
-#endif
+        if (Channel is not null)
+            Channel.Dispose();
     }
 }
-
-#if RL
-[AdminCommand(AdminFlags.Host)]
-sealed class RLReloadCommand : IConsoleCommand
-{
-    public string Command => "reload";
-    public string Description => "Reload RL";
-    public string Help => "reload";
-    
-    public void Execute(IConsoleShell shell, string argStr, string[] args)
-    {
-        var sysMan = IoCManager.Resolve<IEntitySystemManager>();
-        var _rl = sysMan.GetEntitySystem<RLSystem>();
-        if (!_rl.Available())
-        {
-            shell.WriteLine("RL is not available");
-            return;
-        }
-        shell.WriteLine(_rl.Reload());
-    }
-}
-
-[AdminCommand(AdminFlags.Host)]
-sealed class RLEvalCommand : IConsoleCommand
-{
-    public string Command => "eval";
-    public string Description => "Evaluate RL expression";
-    public string Help => "eval <expression>";
-
-    public void Execute(IConsoleShell shell, string argStr, string[] args)
-    {
-        if (args.Length < 1)
-        {
-            shell.WriteLine(Help);
-            return;
-        }
-
-        var sysMan = IoCManager.Resolve<IEntitySystemManager>();
-        var _rl = sysMan.GetEntitySystem<RLSystem>();
-        if (!_rl.Available())
-        {
-            shell.WriteLine("RL is not available");
-            return;
-        }
-
-        var result = RL.eval_str(args[0]);
-        shell.WriteLine(RL.repr(result));
-    }
-}
-
-[AdminCommand(AdminFlags.Host)]
-sealed class RLDebugCommand : IConsoleCommand
-{
-    public string Command => "rldebug";
-    public string Description => "Enable RL debugging";
-    public string Help => "Enable RL debugging";
-
-    public void Execute(IConsoleShell shell, string argStr, string[] args)
-    {
-        RL.set_debug(1);
-    }
-}
-#endif
