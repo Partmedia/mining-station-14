b96fad29f9fb8799e067a80039516d06928b9e4c
Kevin Zheng <kevinz5000@gmail.com>
2023-11-29 18:04:07 -0800

    Shorten camera static

--- Content.Client/SurveillanceCamera/ActiveSurveillanceCameraMonitor.cs
+++ Content.Client/SurveillanceCamera/ActiveSurveillanceCameraMonitor.cs
@@ -3,7 +3,7 @@ namespace Content.Client.SurveillanceCamera;
 [RegisterComponent]
 public sealed class ActiveSurveillanceCameraMonitorVisualsComponent : Component
 {
-    public float TimeLeft = 5f;
+    public float TimeLeft = 2.5f;
 
     public Action? OnFinish;
 }
