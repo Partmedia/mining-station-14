e5397dbd2bb286529eaefc961373b44f7863aff9
Kevin Zheng <kevinz5000@gmail.com>
2023-12-05 16:00:43 -0800

    Add special ladders

--- Content.Server/Warps/WarperComponent.cs
+++ Content.Server/Warps/WarperComponent.cs
@@ -5,4 +5,7 @@ public sealed class WarperComponent : Component
 {
     /// Warp destination unique identifier.
     [ViewVariables(VVAccess.ReadWrite)] [DataField("id")] public string? ID { get; set; }
+
+    /// Generates a dungeon destination upon use.
+    [ViewVariables(VVAccess.ReadWrite)] [DataField("dungeon")] public bool Dungeon = false;
 }
--- Content.Server/Warps/WarperSystem.cs
+++ Content.Server/Warps/WarperSystem.cs
@@ -1,28 +1,86 @@
+using System.Linq;
+
 using Content.Server.Ghost.Components;
 using Content.Server.Popups;
+using Content.Shared.GameTicking;
+using Content.Shared.Gravity;
 using Content.Shared.Interaction;
+using Robust.Server.GameObjects;
 using Robust.Shared.Map;
-using Robust.Shared.Physics;
 using Robust.Shared.Physics.Components;
 using Robust.Shared.Physics.Systems;
+using Robust.Shared.Physics;
 using Robust.Shared.Player;
 
 namespace Content.Server.Warps;
 
 public class WarperSystem : EntitySystem
 {
+    [Dependency] private readonly IEntityManager _entMan = default!;
+    [Dependency] private readonly IMapManager _mapManager = default!;
+    [Dependency] private readonly MapLoaderSystem _map = default!;
     [Dependency] private readonly PopupSystem _popupSystem = default!;
     [Dependency] private readonly SharedPhysicsSystem _physics = default!;
     [Dependency] private readonly WarpPointSystem _warpPointSystem = default!;
+
+    private int dungeonLevel = 0;
 
     public override void Initialize()
     {
         base.Initialize();
         SubscribeLocalEvent<WarperComponent, InteractHandEvent>(OnInteractHand);
+        SubscribeLocalEvent<RoundRestartCleanupEvent>(OnCleanup);
+    }
+
+    private void OnCleanup(RoundRestartCleanupEvent ev)
+    {
+        dungeonLevel = 0;
     }
 
     private void OnInteractHand(EntityUid uid, WarperComponent component, InteractHandEvent args)
     {
+        if (component.Dungeon)
+        {
+            var dMap = _mapManager.CreateMap();
+            var map = _map.LoadMap(dMap, "/Mining/Maps/Templates/dungeon.yml");
+            var gravity = _entMan.EnsureComponent<GravityComponent>(map.First());
+            gravity.Enabled = true;
+            _entMan.Dirty(gravity);
+
+            // create destination back up on current map
+            var upDest = _entMan.SpawnEntity("WarpPoint", Transform(uid).Coordinates);
+            if (TryComp<WarpPointComponent>(upDest, out var upWarp))
+            {
+                upWarp.ID = $"dlvl{dungeonLevel}down";
+            }
+
+            // Destination is next level
+            dungeonLevel++;
+            component.ID = $"dlvl{dungeonLevel}up";
+
+            // find stairs up, create warp destination
+            foreach (var warper in EntityManager.EntityQuery<WarperComponent>())
+            {
+                // find ladder going up
+                if (Transform(warper.Owner).MapID == dMap && !warper.Dungeon)
+                {
+                    // link back upstairs
+                    warper.ID = $"dlvl{dungeonLevel - 1}down";
+
+                    // Create destination downstairs
+                    var downDest = _entMan.SpawnEntity("WarpPoint", Transform(warper.Owner).Coordinates);
+                    if (TryComp<WarpPointComponent>(downDest, out var downWarp))
+                    {
+                        downWarp.ID = $"dlvl{dungeonLevel}up";
+                        downWarp.Location = $"Dungeon Level {dungeonLevel}";
+                    }
+                }
+            }
+
+            // don't generate again
+            component.Dungeon = false;
+        }
+
         if (component.ID is null)
         {
             Logger.DebugS("warper", "Warper has no destination");
