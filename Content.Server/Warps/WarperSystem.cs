c86ee448a96af29bb61c2ec07b8a03db3bfcb487
Kevin Zheng <kevinz5000@gmail.com>
2024-02-19 13:02:01 -0800

    Implement dragging items up/down stairs

--- Content.Server/Warps/WarperSystem.cs
+++ Content.Server/Warps/WarperSystem.cs
@@ -1,8 +1,10 @@
 using System.Linq;
 
 using Content.Server.Chat.Systems;
+using Content.Server.DoAfter;
 using Content.Server.Ghost.Components;
 using Content.Server.Popups;
+using Content.Shared.DragDrop;
 using Content.Shared.GameTicking;
 using Content.Shared.Gravity;
 using Content.Shared.Interaction;
@@ -24,6 +26,7 @@ public class WarperSystem : EntitySystem
     [Dependency] private readonly SharedPhysicsSystem _physics = default!;
     [Dependency] private readonly WarpPointSystem _warpPointSystem = default!;
     [Dependency] private readonly ChatSystem _chatSystem = default!;
+    [Dependency] private readonly DoAfterSystem _doAfter = default!;
 
     public int dungeonLevel = 0;
 
@@ -31,6 +34,8 @@ public class WarperSystem : EntitySystem
     {
         base.Initialize();
         SubscribeLocalEvent<WarperComponent, ActivateInWorldEvent>(OnActivate);
+        SubscribeLocalEvent<WarperComponent, DragDropEvent>(OnDragDrop);
+        SubscribeLocalEvent<WarperComponent, MoveFinished>(OnMoveFinished);
         SubscribeLocalEvent<RoundRestartCleanupEvent>(OnCleanup);
     }
 
@@ -94,10 +99,15 @@ public class WarperSystem : EntitySystem
             RandomAnnounce(uid, dungeonLevel);
         }
 
+        DoWarp(args.Target, args.User, args.User, component);
+    }
+
+    private void DoWarp(EntityUid uid, EntityUid user, EntityUid victim, WarperComponent component)
+    {
         if (component.ID is null)
         {
             Logger.DebugS("warper", "Warper has no destination");
-            _popupSystem.PopupEntity(Loc.GetString("warper-goes-nowhere", ("warper", args.Target)), args.User, Filter.Entities(args.User));
+            _popupSystem.PopupEntity(Loc.GetString("warper-goes-nowhere", ("warper", uid)), user, Filter.Entities(user));
             return;
         }
 
@@ -105,7 +115,7 @@ public class WarperSystem : EntitySystem
         if (dest is null)
         {
             Logger.DebugS("warper", String.Format("Warp destination '{0}' not found", component.ID));
-            _popupSystem.PopupEntity(Loc.GetString("warper-goes-nowhere", ("warper", args.Target)), args.User, Filter.Entities(args.User));
+            _popupSystem.PopupEntity(Loc.GetString("warper-goes-nowhere", ("warper", uid)), user, Filter.Entities(user));
             return;
         }
 
@@ -115,7 +125,7 @@ public class WarperSystem : EntitySystem
         if (destXform is null)
         {
             Logger.DebugS("warper", String.Format("Warp destination '{0}' has no transform", component.ID));
-            _popupSystem.PopupEntity(Loc.GetString("warper-goes-nowhere", ("warper", args.Target)), args.User, Filter.Entities(args.User));
+            _popupSystem.PopupEntity(Loc.GetString("warper-goes-nowhere", ("warper", uid)), user, Filter.Entities(user));
             return;
         }
 
@@ -124,21 +134,58 @@ public class WarperSystem : EntitySystem
         var destMap = destXform.MapID;
         if (!mapMgr.IsMapInitialized(destMap) || mapMgr.IsMapPaused(destMap))
         {
-            if (!entMan.HasComponent<GhostComponent>(args.User))
+            if (!entMan.HasComponent<GhostComponent>(user))
             {
                 // Normal ghosts cannot interact, so if we're here this is already an admin ghost.
                 Logger.DebugS("warper", String.Format("Player tried to warp to '{0}', which is not on a running map", component.ID));
-                _popupSystem.PopupEntity(Loc.GetString("warper-goes-nowhere", ("warper", args.Target)), args.User, Filter.Entities(args.User));
+                _popupSystem.PopupEntity(Loc.GetString("warper-goes-nowhere", ("warper", uid)), user, Filter.Entities(user));
                 return;
             }
         }
 
-        var xform = entMan.GetComponent<TransformComponent>(args.User);
+        var xform = entMan.GetComponent<TransformComponent>(victim);
         xform.Coordinates = destXform.Coordinates;
         xform.AttachToGridOrMap();
-        if (entMan.TryGetComponent(uid, out PhysicsComponent? phys))
+        if (entMan.TryGetComponent(victim, out PhysicsComponent? phys))
         {
-            _physics.SetLinearVelocity(uid, Vector2.Zero);
+            _physics.SetLinearVelocity(victim, Vector2.Zero);
         }
+    }
+
+    private void OnDragDrop(EntityUid uid, WarperComponent component, DragDropEvent args)
+    {
+        if (args.Handled)
+            return;
+
+        args.Handled = true;
+        var userUid = args.User;
+        var doAfterArgs = new DoAfterEventArgs(userUid, 5, default, uid)
+        {
+            BreakOnTargetMove = true,
+            BreakOnUserMove = true,
+            BreakOnDamage = true,
+            BreakOnStun = true,
+            NeedHand = true,
+            TargetFinishedEvent = new MoveFinished(userUid, args.Dragged)
+        };
+
+        _doAfter.DoAfter(doAfterArgs);
+    }
+
+    private sealed class MoveFinished : EntityEventArgs
+    {
+        public EntityUid VictimUid;
+        public EntityUid UserUid;
+
+        public MoveFinished(EntityUid userUid, EntityUid victimUid)
+        {
+            UserUid = userUid;
+            VictimUid = victimUid;
+        }
+    }
+
+    private void OnMoveFinished(EntityUid uid, WarperComponent component, MoveFinished args)
+    {
+        DoWarp(component.Owner, args.UserUid, args.VictimUid, component);
     }
 }
