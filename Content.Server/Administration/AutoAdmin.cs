08ca0cde061434a3516d2e5503b1259838d300ef
Kevin Zheng <kevinz5000@gmail.com>
2024-03-04 12:46:35 -0800

    Improve auto-admin
    Leave better logs and handle players fighting back antags.

--- Content.Server/Administration/AutoAdmin.cs
+++ Content.Server/Administration/AutoAdmin.cs
@@ -3,9 +3,11 @@ using Robust.Server.Player;
 using Robust.Shared.Configuration;
 using Robust.Shared.Network;
 
+using Content.Shared.Administration.Logs;
 using Content.Server.Administration.Systems;
 using Content.Shared.Administration;
 using Content.Shared.CCVar;
+using Content.Shared.Database;
 using Content.Shared.GameTicking;
 
 namespace Content.Server.Administration;
@@ -16,6 +18,7 @@ public sealed class AutoAdmin : EntitySystem, IAutoAdmin
     [Dependency] private readonly IEntityManager _entityManager = default!;
     [Dependency] private readonly IEntitySystemManager _entitySystemManager = default!;
     [Dependency] private readonly IServerNetManager _netManager = default!;
+    [Dependency] protected readonly ISharedAdminLogManager AdminLogger = default!;
 
     private Dictionary<NetUserId, float> record = new Dictionary<NetUserId, float>();
 
@@ -30,25 +33,32 @@ public sealed class AutoAdmin : EntitySystem, IAutoAdmin
         if (!Enabled())
             return;
 
+        // ignore staff of healing(?)
+        if (damage < 0)
+            return;
+
         // attacker is not a player, ignore
         if (!_entityManager.TryGetComponent<ActorComponent>(attacker, out var attackerActor))
             return;
-
-        var attackerId = attackerActor.PlayerSession.UserId;
+        NetUserId attackerId = attackerActor.PlayerSession.UserId;
 
         // victim is not a player, reduce player's RDM score
-        if (!IsPlayer(attacked))
+        if (!_entityManager.TryGetComponent<ActorComponent>(attacked, out var attackedActor))
         {
             Score(attackerId, -damage);
             return;
         }
+        NetUserId attackedId = attackedActor.PlayerSession.UserId;
 
-        // if either attacker or attacked is an antag, there's a reason to fight
-        if (IsAntag(attacker) || IsAntag(attacked))
+        // allow anyone to fight antags
+        if (IsAntag(attackedId))
+        {
+            AdminLogger.Add(LogType.AutoAdmin, LogImpact.Medium, $"{ToPrettyString(attacker)} attacks antag {ToPrettyString(attacked)}");
             return;
+        }
 
         float n = Score(attackerId, damage);
-        Logger.InfoS("autoadmin", $"auto-admin violation {n} for {attackerId}");
+        AdminLogger.Add(LogType.AutoAdmin, LogImpact.High, $"RDM: {ToPrettyString(attacker)} attacks {ToPrettyString(attacked)}");
         AdminAction(attackerActor.PlayerSession, n);
     }
 
@@ -74,22 +84,26 @@ public sealed class AutoAdmin : EntitySystem, IAutoAdmin
             // kick
             _netManager.DisconnectChannel(session.ConnectedClient, Loc.GetString("autoadmin-kick"));
             _bwoinkSystem.Bwoink(session.UserId, Loc.GetString("autoadmin-kick"));
+            AdminLogger.Add(LogType.AutoAdmin, LogImpact.High, $"kicking ${session.UserId}");
         }
         else if (n >= _cfg.GetCVar(CCVars.AutoWarnThresh))
         {
             // warn
             _bwoinkSystem.Bwoink(session.UserId, Loc.GetString("autoadmin-no-rdm"));
+            AdminLogger.Add(LogType.AutoAdmin, LogImpact.High, $"warning ${session.UserId}");
         }
     }
 
-    private bool IsPlayer(EntityUid uid)
+    private bool IsAntag(NetUserId uid)
     {
-        return _entityManager.HasComponent<ActorComponent>(uid);
-    }
-
-    private bool IsAntag(EntityUid uid)
-    {
-        return false; // FIXME
+        if (record.TryGetValue(uid, out var score))
+        {
+            if (score >= _cfg.GetCVar(CCVars.AutoWarnThresh))
+            {
+                return true;
+            }
+        }
+        return false;
     }
 
     private bool Enabled()
